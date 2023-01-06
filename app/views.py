d2df3cf363e28f6101e197107993863734f70927
timmySpark <timmyspark1@gmail.com>
2023-01-06 20:40:52 +0000

    [feat] :- creating views and urls for django app 'app'

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    template_name = 'index.html'
+    content = {}
+    return render(request, template_name, content)
+
+
+def encode(request):
+    template_name = 'encode.html'
+    content = {}
+    return render(request, template_name, content)
