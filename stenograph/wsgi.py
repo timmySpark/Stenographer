c9bed32c359f8717e134aa71c1b693fa1f3892bd
Gorgeous T <tomisinerinle4@gmail.com>
2023-04-08 19:00:52 +0100

    Added Vercel deployment requirement

--- stenograph/wsgi.py
+++ stenograph/wsgi.py
@@ -5,7 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
+# """
 
 import os
 
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stenograph.settings')
 
 application = get_wsgi_application()
+
+app = application
