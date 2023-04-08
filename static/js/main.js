b7fd4017d2b0aec960cc8a5faaad1c38602b3468
Gorgeous T <tomisinerinle4@gmail.com>
2023-04-08 19:07:53 +0100

    Added functions for result display

--- static/js/main.js
+++ static/js/main.js
@@ -23,13 +23,49 @@
 
 // selectInput
 
+// Prvent reload
+// const onConfirmRefresh = function (event) {
+//     event.preventDefault();
+//     alert('here')
+//     return event.returnValue = "Are you sure you want to leave the page?";
+//   }
+  
+// window.addEventListener("beforeunload", onConfirmRefresh, { capture: true });
 
 function hideShow(hideElem, showElem){
     $(`.${hideElem}`).css("display", "none");
     $(`.${showElem}`).css("display", "flex");
 }
 
+function switchImg(elem, image){
+    // alert(image)
+    toggleText = document.getElementsByClassName('image-toggle-text')
+    original_img = document.getElementsByClassName('original_img')[0]
+    encoded_img = document.getElementsByClassName('encoded_img')[0]    
 
+    for (let index = 0; index < toggleText.length; index++) {
+        // if (elem != toggleText[index].classList.contains('active')){
+        if (toggleText[index].classList.contains('active')){
+            toggleText[index].classList.remove('active')
+        }
+        else{
+            toggleText[index].classList.add('active')
+        }
+        // }
+     
+    }
+
+    if (image == 'encoded'){
+        encoded_img.style.display = 'inline-block'
+        original_img.style.display = 'none'
+    }
+
+    else{
+        original_img.style.display = 'inline-block'
+        encoded_img.style.display = 'none'
+    }
+   
+}
 // how it works toggle
 
 function guideToggler(user){
@@ -127,4 +163,21 @@ function urlPrompt(opt) {
         
        
     }
+}
+
+function downloadImg() {
+    downloadTag = document.getElementsByClassName('download-link')[0]
+    // modal = document.getElementsByClassName('download-failed')[0]
+
+    $.get(downloadTag.href)
+        .done(function() { 
+            downloadTag.click()
+            // $('.download-failed').modal('show')
+
+        })
+
+        .fail(function() { 
+            $('.download-failed').modal('show')
+        })
+
 }
